1d28dc5f517c1f27ee739a0e63f18f59c443ae81
Tudor-FG <128051147+Tudor-FG@users.noreply.github.com>
2023-05-18 21:12:46 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -8,19 +8,19 @@ var nav = document.querySelector('.nav');
 				sliptAll[i].setAttribute('data-index', i);
 			}
 
-			// // 点击
-			// for (let i = 0; i < sliptAll.length; i++) {
-			// 	sliptAll[i].onclick = function (e) {
-			// 		//排他思想
-			// 		for (let j = 0; j < sliptAll.length; j++) {
-			// 			sliptAll[j].classList.remove('selected');
-			// 		}
-			// 		// this.style.color = '#FFF';
-			// 		this.classList.add('selected'); // 通过添加类名实现颜色变化
-			// 		let len = 155 * (4 - i) + 767; // 计算背景滑块left值
-			// 		backblock.style.left = len + 'px';
-			// 	};
-			// }
+			// 点击
+			for (let i = 0; i < sliptAll.length; i++) {
+				sliptAll[i].onclick = function (e) {
+					//排他思想
+					for (let j = 0; j < sliptAll.length; j++) {
+						sliptAll[j].classList.remove('selected');
+					}
+					// this.style.color = '#FFF';
+					this.classList.add('selected'); // 通过添加类名实现颜色变化
+					let len = 155 * (4 - i) + 767; // 计算背景滑块left值
+					backblock.style.left = len + 'px';
+				};
+			}
 
 			// 鼠标经过
 			for (let i = 0; i < sliptAll.length; i++) {
